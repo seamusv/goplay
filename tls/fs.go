e27957b16b1a2c6b07bf930d7b5474160c23f381
Seamus Venasse <svenasse@gmail.com>
2023-07-27 09:37:50 -0700

    Update TLS configuration to be compatible with the AWS recommendation of `ELBSecurityPolicy-TLS13-1-2-2021-06`

--- tls/fs.go
+++ tls/fs.go
@@ -45,20 +45,25 @@ func FS(certsFS fs.ReadFileFS) (*tls.Config, error) {
 		return nil, err
 	}
 
+	// Documentation: https://wiki.mozilla.org/Security/Server_Side_TLS
 	tlsConfig := &tls.Config{
 		Certificates: certificates,
 		MinVersion:   tls.VersionTLS12,
 		CurvePreferences: []tls.CurveID{
+			tls.CurveP384,
 			tls.CurveP256,
 			tls.X25519,
 		},
 		CipherSuites: []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		},
 	}
 	return tlsConfig, nil
