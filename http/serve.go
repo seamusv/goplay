9fa7e2fa0ffb4ad06bb42e77881d28656ee9d686
Seamus Venasse <svenasse@gmail.com>
2025-05-29 12:12:07 -0700

    Change priority of curves and ciphers

--- http/serve.go
+++ http/serve.go
@@ -102,20 +102,20 @@ func (s *Server) Serve(tlsHandler http.Handler, handler http.Handler) error {
 				acme.ALPNProto, // enable tls-alpn ACME challenges
 			},
 			CurvePreferences: []tls.CurveID{
-				tls.CurveP384,
-				tls.CurveP256,
 				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
 			},
 			CipherSuites: []uint16{
 				tls.TLS_AES_128_GCM_SHA256,
-				tls.TLS_AES_256_GCM_SHA384,
 				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			},
 		}
 
